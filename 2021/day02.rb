c60c75514295c775cb2cf42f8f040e64fe7ba913
Brandon Autrey <brandon.autrey@glia.com>
2021-12-02 23:39:03 +0200

    Complete day 02

---
+++ 2021/day02.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+# Part 1
+horizontal = 0
+depth = 0
+
+File.foreach('day02-input.txt') do |command|
+  line = command.split
+
+  case line[0]
+  when 'forward'
+    horizontal += line[1].to_i
+  when 'up'
+    depth -= line[1].to_i
+  when 'down'
+    depth += line[1].to_i
+  end
+end
+
+puts "Part 1: #{depth * horizontal}"
+
+# Part 2
+
+horizontal = 0
+depth = 0
+aim = 0
+
+File.foreach('day02-input.txt') do |command|
+  line = command.split
+  value = line[1].to_i
+
+  case line[0]
+  when 'forward'
+    horizontal += value
+    depth += value * aim
+  when 'up'
+    aim -= value
+  when 'down'
+    aim += value
+  end
+end
+
+puts "Part 2: #{depth * horizontal}"
